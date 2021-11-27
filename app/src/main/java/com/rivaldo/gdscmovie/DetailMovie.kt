c557cc6085c57c5502830a4ee78fd9861a62120e
RivaldoTjio <rivaldohadiwinata@gmail.com>
2021-11-27 14:03:25 +0700

    Initial commit

---
+++ app/src/main/java/com/rivaldo/gdscmovie/DetailMovie.kt
@@ -0,0 +1,11 @@
+package com.rivaldo.gdscmovie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailMovie : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_movie)
+    }
+}
